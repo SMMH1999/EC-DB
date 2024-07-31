84fa44f8b95dcee13dbd2b7654818b6e8941d44c
SMMH <osonhast@gmail.com>
2024-07-31 12:01:56 +0330

    structure #

---
+++ Backend/model.py
@@ -0,0 +1,8 @@
+
+class Client:
+    def __init__(self, f_name, l_name, username, password) -> None:
+        self.f_name = f_name
+        self.l_name = l_name
+        self.username = username
+        self.password = password
+        
